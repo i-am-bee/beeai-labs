562fbc674e37a88169eb96c24fc1f4800285d825
Nigel Jones <jonesn@uk.ibm.com>
2025-01-14 16:45:57 +0000

    improve: Add initial observability support
    Signed-off-by: Nigel Jones <jonesn@uk.ibm.com>

--- bee-hive/bee_hive/run_workflow.py
+++ bee-hive/bee_hive/run_workflow.py
@@ -10,9 +10,10 @@ import dotenv
 from openai import AssistantEventHandler, OpenAI
 from openai.types.beta import AssistantStreamEvent
 from openai.types.beta.threads.runs import RunStep, RunStepDelta, ToolCall
+from opentelemetry import trace
+from opentelemetry import context
 
 dotenv.load_dotenv()
-
 
 def parse_yaml(file_path):
     with open(file_path, "r") as file:
@@ -42,34 +43,38 @@ def sequential_workflow(workflow):
 
 
 def run_agent(agent_name, prompt):
-    print(f"🐝 Running {agent_name}...")
-    agent_id = load_agent(agent_name)
-    client = OpenAI(
-        base_url=f'{os.getenv("BEE_API")}/v1', api_key=os.getenv("BEE_API_KEY")
-    )
-    assistant = client.beta.assistants.retrieve(agent_id)
-    thread = client.beta.threads.create(
-        messages=[{"role": "user", "content": str(prompt)}]
-    )
-    client.beta.threads.runs.create_and_poll(
-        thread_id=thread.id, assistant_id=assistant.id
-    )
-    messages = client.beta.threads.messages.list(thread_id=thread.id)
-    answer = messages.data[0].content[0].text.value
-    print(f"🐝 Response from {agent_name}: {answer}")
-    return answer
+    with tracer.start_as_current_span("parent", attributes={"agent.name": agent_name}):
+        print(f"🐝 Running {agent_name}...")
+    
+        agent_id = load_agent(agent_name)
+        client = OpenAI(
+            base_url=f'{os.getenv("BEE_API")}/v1', api_key=os.getenv("BEE_API_KEY")
+        )
+        assistant = client.beta.assistants.retrieve(agent_id)
+        thread = client.beta.threads.create(
+            messages=[{"role": "user", "content": str(prompt)}]
+        )
+        client.beta.threads.runs.create_and_poll(
+            thread_id=thread.id, assistant_id=assistant.id
+        )
+        messages = client.beta.threads.messages.list(thread_id=thread.id)
+        answer = messages.data[0].content[0].text.value
+        print(f"🐝 Response from {agent_name}: {answer}")
+        return answer
 
 
 def run_streaming_agent(agent_name, prompt):
-    print(f"🐝 Running {agent_name}...")
-    agent_id = load_agent(agent_name)
-    client = OpenAI(
-        base_url=f'{os.getenv("BEE_API")}/v1', api_key=os.getenv("BEE_API_KEY")
-    )
-    assistant = client.beta.assistants.retrieve(agent_id)
-    thread = client.beta.threads.create(
-        messages=[{"role": "user", "content": str(prompt)}]
-    )
+    with tracer.start_as_current_span("parent", attributes={"agent.name": agent_name}):
+        print(f"🐝 Running {agent_name}...")
+        
+        agent_id = load_agent(agent_name)
+        client = OpenAI(
+            base_url=f'{os.getenv("BEE_API")}/v1', api_key=os.getenv("BEE_API_KEY")
+        )
+        assistant = client.beta.assistants.retrieve(agent_id)
+        thread = client.beta.threads.create(
+            messages=[{"role": "user", "content": str(prompt)}]
+        )
 
     class EventHandler(AssistantEventHandler):
         """NOTE: Streaming is work in progress, not all methods are implemented"""
@@ -104,18 +109,56 @@ def run_streaming_agent(agent_name, prompt):
     print(f"🐝 Response from {agent_name}: {answer}")
     return answer
 
+# TODO: Part of python framework?
+def setup_opentelemetry():
+    from opentelemetry.sdk.resources import SERVICE_NAME, Resource
+
+    from opentelemetry import trace
+    from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
+    from opentelemetry.sdk.trace import TracerProvider
+    from opentelemetry.sdk.trace.export import BatchSpanProcessor,ConsoleSpanExporter
+
+    from opentelemetry import metrics
+    from opentelemetry.exporter.otlp.proto.http.metric_exporter import OTLPMetricExporter
+    from opentelemetry.sdk.metrics import MeterProvider
+    from opentelemetry.sdk.metrics.export import PeriodicExportingMetricReader,ConsoleMetricExporter
+
+    # Service name is required for most backends
+    resource = Resource(attributes={
+        SERVICE_NAME: "bee-hive"
+    })
+
+    # TODO: Get endpoints from config
+    traceProvider = TracerProvider(resource=resource)
+    processor = BatchSpanProcessor(OTLPSpanExporter(endpoint="http://localhost:4318/v1/traces",headers={"x-bee-authorization":"observe-auth-key"}))
+    #processor=BatchSpanProcessor(span_exporter=ConsoleSpanExporter())
+    traceProvider.add_span_processor(processor)
+    trace.set_tracer_provider(traceProvider)
+
+    reader = PeriodicExportingMetricReader(
+        OTLPMetricExporter(endpoint="http://localhost:4318/v1/metrics",headers={"x-bee-authorization":"observe-auth-key"})
+    )
+    # observe-auth-key
+    #reader=PeriodicExportingMetricReader(ConsoleMetricExporter())
+    meterProvider = MeterProvider(resource=resource, metric_readers=[reader])
+    metrics.set_meter_provider(meterProvider)
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage: python run_workflow.py <yaml_file>")
         sys.exit(1)
+        
+    setup_opentelemetry()
+    # initialize trace
+    # TODO: should we use same module name as framework?
+    tracer = trace.get_tracer("bee-hive")
+    with tracer.start_as_current_span("workflow"):
+        file_path = sys.argv[1]
+        workflow_yaml = parse_yaml(file_path)
+        runtime_strategy = workflow_yaml["spec"]["strategy"]["type"]
 
-    file_path = sys.argv[1]
-    workflow_yaml = parse_yaml(file_path)
-    runtime_strategy = workflow_yaml["spec"]["strategy"]["type"]
-
-    if runtime_strategy == "sequence":
-        result = sequential_workflow(workflow_yaml)
-        print(f"🐝 Final answer: {result}")
-    else:
-        raise ValueError("Invalid workflow strategy type: ", runtime_strategy)
+        if runtime_strategy == "sequence":
+            result = sequential_workflow(workflow_yaml)
+            print(f"🐝 Final answer: {result}")
+        else:
+            raise ValueError("Invalid workflow strategy type: ", runtime_strategy)
