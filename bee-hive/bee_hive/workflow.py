48f6d03951d832791209b43ddb6665cef39f12de
Akihiko Kuroda <akihikokuroda2020@gmail.com>
2025-01-16 16:16:37 -0500

    update workflow.py
    Signed-off-by: Akihiko Kuroda <akihikokuroda2020@gmail.com>

--- bee-hive/bee_hive/workflow.py
+++ bee-hive/bee_hive/workflow.py
@@ -11,22 +11,35 @@ import dotenv
 dotenv.load_dotenv()
 
 class Workflow:
-def __init__(self, workflow)
+    agents = []
+    def __init__(self, agent_defs)
+        """Execute sequential workflow.
+        input:
+            agents: array of agent definitions
+        """
+        for agent_def in agent_defs:
+            agents.append(new Agent(agent_def))
 
 
-def sequential_workflow(workflow):
-    prompt = workflow["spec"]["template"]["prompt"]
-    agents = workflow["spec"]["template"]["agents"]
-    if (
-        workflow_yaml["spec"]["strategy"]["output"]
-        and workflow_yaml["spec"]["strategy"]["output"] == "verbose"
-    ):
-        run_workflow = run_streaming_agent
-    else:
-        run_workflow = run_agent
-    for agent in agents:
-        prompt = run_workflow(agent[agent, prompt)
-    return prompt
+    def sequential_workflow(workflow):
+        """Execute sequential workflow.
+        input: workflow
+            workflow["spec"]["template"]["prompt"]: prompt for the agent
+            workflow["spec"]["template"]["agents"]: array of agent instance
+        """
+    
+        prompt = workflow["spec"]["template"]["prompt"]
+        agents = workflow["spec"]["template"]["agents"]
+        if (
+            workflow_yaml["spec"]["strategy"]["output"]
+            and workflow_yaml["spec"]["strategy"]["output"] == "verbose"
+           ):
+            run_workflow = run_streaming_agent
+        else:
+            run_workflow = run_agent
+        for agent in agents:
+            prompt = run_workflow(agent[agent, prompt)
+        return prompt
 
 
 
