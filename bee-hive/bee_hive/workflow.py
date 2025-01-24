2c886bd5760ed1f0863000734f987138a3a82712
George Liu <gliu@Georges-MacBook-Pro.local>
2025-01-24 07:58:51 -0800

    fixed loop as no need to get name/values, removed comments, fixed verbose

--- bee-hive/bee_hive/workflow.py
+++ bee-hive/bee_hive/workflow.py
@@ -38,28 +38,20 @@ class Workflow:
             print("not supported yet")   
 
     def _sequence(self):
-        # NOTE: "template" is an arbitrary key name that we use for now, it could be anything
         prompt = self.workflow["spec"]["template"]["prompt"]
         steps = self.workflow["spec"]["template"]["steps"]
-
-        # Create a Step object for each step in the workflow
         for step in steps:
             if step["agent"]:
                 step["agent"] = self.agents.get(step["agent"])
             self.steps[step["name"]] = Step(step)
-
-        # Run each step in sequence, assuming that the output of each step is the input to the next
-        for step_name in self.steps:
-            step = self.steps[step_name]
+        for step in self.steps:
             response = step.run(prompt)
             prompt = response.get("prompt", prompt)
-
         if (
             self.workflow["spec"]["strategy"]["output"]
             and self.workflow["spec"]["strategy"]["output"] == "verbose"
         ):
-            print(f"Step {step_name} output: {response}")
-
+            print(f"Step {step.step_name} output: {response}")
         return prompt
 
     def _condition(self):
