f9265135952ed7a8c8afc156a1b62d6ad47b51aa
Akihiko Kuroda <akihikokuroda2020@gmail.com>
2025-02-21 14:16:18 -0500

    deploy CI tests
    Signed-off-by: Akihiko Kuroda <akihikokuroda2020@gmail.com>

---
+++ maestro/tests/integration/test_deploy.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+
+# Copyright © 2025 IBM
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+import subprocess
+import requests
+
+import pytest
+from unittest import TestCase
+from src.deploy import Deploy
+
+# `deploy` tests
+class test_deploy(TestCase):
+    def setUp(self):        
+        self.cwd = os.getcwd()
+        os.chdir("src")
+        self.deploy = Deploy("../tests/examples/condition_agents.yaml", "../tests/examples/condition_workflow.yaml", "BEE_API_KEY=sk-proj-testkey BEE_API=http://192.168.86.45:4000 DRY_RUN=1")
+        #self.deploy = Deploy("../tests/examples/condition_agents.yaml", "../tests/examples/condition_workflow.yaml", "BEE_API_KEY=sk-proj-testkey BEE_API=http://192.168.86.45:4000")
+        
+    def tearDown(self):
+        self.deploy = None
+        os.chdir(self.cwd)
+
+    @pytest.mark.skipif(os.getenv('DEPLOY_KUBERNETES_TEST') != "1", reason="Kubernetes deploy skipped")
+    def test_deploy_to_kubernetes(self):
+        self.deploy.deploy_to_kubernetes()
+        response = requests.get("http://127.0.0.1:30051/").text
+        self.assertTrue(response.find("Running expert...") != -1)
+        self.assertTrue(response.find("Running colleague...") != -1)
+
+    @pytest.mark.skipif(os.getenv('DEPLOY_DOCKER_TEST') != "1", reason="Docker deploy skipped")
+    def test_deploy_to_docker(self):
+        self.deploy.deploy_to_docker()
+        response = requests.get("http://127.0.0.1:30051/").text
+        self.assertTrue(response.find("Running expert...") != -1)
+        self.assertTrue(response.find("Running colleague...") != -1)
+
+if __name__ == '__main__':
+    unittest.main()
+
+#deploy = Deploy("../tests/examples/condition_agents.yaml", "../tests/examples/condition_workflow.yaml", "BEE_API_KEY=sk-proj-testkey BEE_API=http://192.168.86.45:4000", "127.0.0.1:5000")
+#deploy.deploy_to_docker()
+#deploy = Deploy("../tests/examples/condition_agents.yaml", "../tests/examples/condition_workflow.yaml", "BEE_API_KEY=sk-proj-testkey BEE_API=http://192.168.86.45:4000 DRY_RUN=1")
+#deploy.deploy_to_kubernetes()
