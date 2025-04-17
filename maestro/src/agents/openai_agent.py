7a916ede6b74e6f84aa3dfafe993b93e9a963ec4
Nigel Jones <jonesn@uk.ibm.com>
2025-04-17 18:16:02 +0100

    OpenAI support - support Ollama
    Signed-off-by: Nigel Jones <jonesn@uk.ibm.com>

--- maestro/src/agents/openai_agent.py
+++ maestro/src/agents/openai_agent.py
@@ -12,9 +12,15 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import os
+# TODO Only agent *needs* aliasing
 from agents import (
     Agent as OAIAgent,
-    Runner as OAIRunner
+    Runner as OAIRunner,
+    AsyncOpenAI as OAIAsyncOpenAI,
+    set_default_openai_api,
+    set_default_openai_client,
+    set_tracing_disabled,
 )
 from src.agents.agent import Agent
 
@@ -32,10 +38,31 @@ class OpenAIAgent(Agent):
         super().__init__(agent)
         
         # TODO: Add tools (std and MCP)
+
+        # TODO : for now we use environment variables - set to http://localhost:11434/v1 for Ollama
+        base_url = os.getenv("OPENAI_BASE_URL","https://api.openai.com/v1")
+        api_key = os.getenv("OPENAI_API_KEY")
+        spec_dict = agent.get('spec', {})
+        model_name: str = spec_dict.get('model', "gpt-4o-mini")
+
+        # TODO: Proper debug
+        print(f"DEBUG [OpenAIAgent {self.agent_name}]: Using Base URL: {base_url}")
+        print(f"DEBUG [OpenAIAgent {self.agent_name}]: Using Model: {model_name}")
+        
+        client = OAIAsyncOpenAI(
+            base_url=base_url,
+            api_key=api_key,
+        )
+        # Update the parms for the endpoint. Tracing uses OpenAI backend, so disable tracing too
+        set_default_openai_client(client=client, use_for_tracing=False)
+        # For now, use the chat completions api, even for OpenAI
+        set_default_openai_api("chat_completions")
+        set_tracing_disabled(disabled=True)
         
         self.openai_agent = OAIAgent(
             name=self.agent_name,
             instructions=self.instructions,
+            model=model_name
             )
 
         self.agent_id=self.agent_name
