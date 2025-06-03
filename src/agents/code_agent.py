36ef0e25e26018b1fe777bd776ae34f7a7f857ed
Akihiko Kuroda <akihikokuroda2020@gmail.com>
2025-06-03 14:36:23 -0400

    add codeagent
    Signed-off-by: Akihiko Kuroda <akihikokuroda2020@gmail.com>

---
+++ src/agents/code_agent.py
@@ -0,0 +1,53 @@
+#! /usr/bin/env python3
+# SPDX-License-Identifier: Apache-2.0
+
+import asyncio
+import os
+from dotenv import load_dotenv
+from slack_sdk import WebClient
+from slack_sdk.errors import SlackApiError
+
+from src.agents.agent import Agent
+
+load_dotenv()
+
+class CodeAgent(Agent):
+    """
+    CodeAgent extends the Agent class that executes an arbitrary python code specifed in the code section of the agent difinition.
+    """
+
+    def __init__(self, agent: dict) -> None:
+        """
+        Initializes the agent with agent definitions.
+        """
+        super().__init__(agent)
+        
+
+    async def run(self, prompt: str) -> str:
+        """
+        Runs the BeeAI agent with the given prompt.
+        Args:
+            prompt (str): The prompt to run the agent with.
+        """
+
+        self.print(f"Running {self.agent_name} with {prompt}...\n")
+        local = {"input": prompt}
+        exec(self.agent_code, local)
+        answer = local["input"]
+        self.print(f"Response from {self.agent_name}: {answer}\n")        
+        return local["input"]
+
+    async def run_streaming(self, prompt: str) -> str:
+        """
+        Runs the BeeAI agent with the given prompt.
+        Args:
+            prompt (str): The prompt to run the agent with.
+        """
+
+        self.print(f"Running {self.agent_name} with {prompt}...\n")
+        local = {"input": prompt}
+        exec(self.agent_code, local)
+        answer = local["input"]
+        self.print(f"Response from {self.agent_name}: {answer}\n")        
+        return local["input"]
+
